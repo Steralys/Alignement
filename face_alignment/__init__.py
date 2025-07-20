34cd4748963044872f251f39250bb6a38892767e
Steralys <124307617+Steralys@users.noreply.github.com>
2025-07-20 14:23:36 +0200

    Update __init__.py

--- face_alignment/__init__.py
+++ face_alignment/__init__.py
@@ -4,4 +4,4 @@ __author__ = """Adrian Bulat"""
 __email__ = 'adrian@adrianbulat.com'
 __version__ = '1.4.2'
 
-from .api import FaceAlignment, LandmarksType, NetworkSize
+from .api import FaceAlignment
